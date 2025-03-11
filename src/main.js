ec1d8485cc208f4d4adfd744d6ac54f8f2a5fcb0
NouriHicham <nourichamidhicham@fpllefia.com>
2025-03-11 10:14:43 +0100

    Componente enrutador.js

--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
-import { header } from './componentes/header'
-import { footer } from './componentes/footer'
+import { header } from "./componentes/header";
+import { footer } from "./componentes/footer";
+// Import all of Bootstrap's JS
+import "bootstrap";
+// Import our custom CSS
+import "./scss/styles.scss";
 
-import './scss/styles.scss'
+// Importamos la Función para detectar eventos al cargar las vistas
+import { enrutador } from "./componentes/enrutador";
 
-// Importamos la vista por defecto (que será home)
-async function cargarVista () {
-  const componente = await import('./vistas/registroVista')
-  const vista = componente.default
-  // Inyectamos la vista home
-  document.querySelector('main').innerHTML = vista.template
-  // Ejecutamos la lógica de la vista
-  vista.script()
-}
-cargarVista()
+//Inyectamos el componente header
+document.querySelector("header").innerHTML = header.template;
+header.script()
+//Inyectamos el componente footer
+document.querySelector("footer").innerHTML = footer.template;
 
-// Inyectamos el componente header
-document.querySelector('header').innerHTML = header.template
-
-// Inyectamos el componente footer
-document.querySelector('footer').innerHTML = footer.template
+enrutador.observadorRutas();
+// Cargamos la página home
+window.location = "#/home";
