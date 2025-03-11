3153a282d493974ebe24ea2ea9e66e45630066b4
NouriHicham <nourichamidhicham@fpllefia.com>
2025-03-11 09:56:11 +0100

    Componentes y vistas

--- src/main.js
+++ src/main.js
@@ -1,21 +1,21 @@
 import { header } from './componentes/header'
 import { footer } from './componentes/footer'
-// Import all of Bootstrap's JS
-import 'bootstrap'
-// Import our custom CSS
+
 import './scss/styles.scss'
 
 // Importamos la vista por defecto (que será home)
 async function cargarVista () {
-  const componente = await import('./vistas/homeVista')
+  const componente = await import('./vistas/registroVista')
   const vista = componente.default
   // Inyectamos la vista home
   document.querySelector('main').innerHTML = vista.template
+  // Ejecutamos la lógica de la vista
+  vista.script()
 }
 cargarVista()
 
-//Inyectamos el componente header
+// Inyectamos el componente header
 document.querySelector('header').innerHTML = header.template
 
-//Inyectamos el componente footer
+// Inyectamos el componente footer
 document.querySelector('footer').innerHTML = footer.template
